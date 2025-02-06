2cb669b7822f6a27fd65649955a871d4552edab4
tienphuckx <tienphuckx@gmail.com>
2025-02-06 14:34:36 +0700

    init: fund transfer project

---
+++ src/main/java/com/tienphuckx/fundtransfer/FundTransferApplication.java
@@ -0,0 +1,13 @@
+package com.tienphuckx.fundtransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundTransferApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FundTransferApplication.class, args);
+    }
+
+}
